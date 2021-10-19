0e77430ad431e579b1beae0aa3d530f698e83ac2
y-uchiida <y.uchiida@nirneege.com>
2021-10-20 07:03:51 +0900

    fix some files for initialize DB

---
+++ DB_access_sample/Models/Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿namespace DB_access_sample.Models.Data
+{
+    /* データベースの初期化を行うクラス？ */
+    public class DbInitializer
+    {
+        /* データベースが存在しない場合に実行される処理 Initialize 
+         * これが最初に実行されると、データベースが作成される
+         */
+        public static void Initialize(MyDbContext context)
+        {
+            /* 引数で渡されるcontextそって、データベースを初期化する(DI) */
+            context.Database.EnsureCreated();
+        }
+    }
+}
--- DB_access_sample/Program.cs
+++ DB_access_sample/Program.cs
@@ -7,13 +7,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using DB_access_sample.Models.Data; /* データベースコンテキストを参照するため追加 */
+using Microsoft.Extensions.DependencyInjection; /* CreateScope() メソッドを利用するために必要 */
+
 namespace DB_access_sample
 {
     public class Program
     {
+        /* プログラムのエントリーポイント */
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            /* 初期設定だと、すぐにRun() するようになっているので、これを修正する
+             * CreateHostBuilder(args).Build().Run();
+             */
+
+            /* ホストのオブジェクトを作っておき、処理が終わったらRunする */
+            var host = CreateHostBuilder(args).Build();
+
+            /* データベースの初期化処理を設定 */
+            using (var scope = host.Services.CreateScope()) 
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    /* MyDbContextのデータを取得して、それをInitialize に渡す(DI) */
+                    var context = services.GetRequiredService<MyDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                { 
+                    /* 例外処理、ログの保存だけ行う */
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
